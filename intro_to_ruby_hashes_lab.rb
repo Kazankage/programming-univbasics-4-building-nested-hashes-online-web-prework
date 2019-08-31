37386329db236335a976396048a3021f3e8fbcbc
Randel Jiao <randeljiao@gmail.com>
2019-08-31 16:41:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,7 @@ def monopoly_with_fourth_tier
           :three_pieces_owned => 100,
           :four_pieces_owned => 200, },
       :names => {
-          :reading_railroad => {},
+          :reading_railroad => {:mogate},
           :pennsylvania_railroad => {},
           :b_and_o_railroad => {},
           :shortline_railroad => {}
