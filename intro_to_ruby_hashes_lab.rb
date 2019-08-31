d17f0066863667ec1869fc771f3d87d21c3a6360
Randel Jiao <randeljiao@gmail.com>
2019-08-31 16:47:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
       :pieces => 4
      }
     }
-  }
+  
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
         
       }
     }
-      }
+      
     
   
   # When you start writing the implementation for this method, copy the Hash
